5c18ed2c55cd5595984fc15762d86ec65d7b8353
Carl Shelbourne <cws-thg1@theintoy.com>
2016-11-29 10:28:16 +0000

    cleaned up the main app.

--- src/main/java/org/shelbourne/adam/gravity1/GravityApp.java
+++ src/main/java/org/shelbourne/adam/gravity1/GravityApp.java
@@ -1,14 +1,20 @@
 package org.shelbourne.adam.gravity1;
 
 /**
- * Hello world!
+ * Main App for Gravity calculations.
  *
  */
-public class GravityApp 
-{
-    public static void main( String[] args )
-    {
-      GravityScanner gs = new GravityScanner();
-      ScannerDto scannerDto = gs.getFromScanner(false);
-    }
+public class GravityApp {
+
+	private void performCalculate() {
+		GravityScanner gs = new GravityScanner();
+		ScannerDto scannerDto = gs.getFromScanner(false);
+		Projectile projectile = new Projectile(scannerDto);
+		projectile.calculate();
+	}
+
+	public static void main(String[] args) {
+		GravityApp ga = new GravityApp();
+		ga.performCalculate();
+	}
 }
