e972dd331b97062fc41e9d1a342a42376ff1d624
shelly1296 <a.shelbourne@lancaster.ac.uk>
2016-11-30 20:41:04 +0000

    Need this class for it to compile

---
+++ src/main/java/org/shelbourne/adam/gravity1/CalculateGravitaionalField.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package org.shelbourne.adam.gravity1;
+
+/**
+ * @author gamep_000
+ *
+ */
+public class CalculateGravitaionalField {
+	
+	private static final Double GRAVITATIONAL_CONSTANT = 6.67408E-11;
+	private static final Double EARTH_MASS = 5.974E24;
+	private static final Double EARTH_Radius = 6380E3;
+	
+	public CalculateGravitaionalField()  {
+		
+	}
+	
+	
+	public Double calulateGravitationalField (Double x, Double y) {
+		
+		Double radius2 = Math.pow(EARTH_Radius+y, 2) + Math.pow(x, 2);
+		Double radius = Math.pow(radius2, 0.5);
+		Double g = -(GRAVITATIONAL_CONSTANT*EARTH_MASS)/Math.pow(radius, 2); 
+		
+		return g;
+	}
+
+}
