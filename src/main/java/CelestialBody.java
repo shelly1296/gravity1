9dd5a20c95c09a8bdb4611b99b98c7e02b1491d6
Adam Shelbourne <a.shelbourne@lancaster.ac.uk>
2016-12-08 22:25:28 +0000

    Initial CelestialBody

---
+++ src/main/java/CelestialBody.java
@@ -0,0 +1,77 @@
+
+public class CelestialBody {
+	private PlanetData planetData;
+
+	private double currentXPosition;
+	private double currentYPosition;
+	private double currentZPosition;
+
+	private double currentXVelocity;
+	private double currentYVelocity;
+	private double currentZVelocity;
+	
+	public CelestialBody(PlanetData planetData) {
+		this.planetData = planetData;
+		
+		setCurrentXPosition(planetData.getXPosition());
+	}
+	
+	
+
+	public PlanetData getPlanetData() {
+		return planetData;
+	}
+
+	public void setPlanetData(PlanetData planetData) {
+		this.planetData = planetData;
+	}
+
+	public double getCurrentXPosition() {
+		return currentXPosition;
+	}
+
+	public void setCurrentXPosition(double currentXPosition) {
+		this.currentXPosition = currentXPosition;
+	}
+
+	public double getCurrentYPosition() {
+		return currentYPosition;
+	}
+
+	public void setCurrentYPosition(double currentYPosition) {
+		this.currentYPosition = currentYPosition;
+	}
+
+	public double getCurrentZPosition() {
+		return currentZPosition;
+	}
+
+	public void setCurrentZPosition(double currentZPosition) {
+		this.currentZPosition = currentZPosition;
+	}
+
+	public double getCurrentXVelocity() {
+		return currentXVelocity;
+	}
+
+	public void setCurrentXVelocity(double currentXVelocity) {
+		this.currentXVelocity = currentXVelocity;
+	}
+
+	public double getCurrentYVelocity() {
+		return currentYVelocity;
+	}
+
+	public void setCurrentYVelocity(double currentYVelocity) {
+		this.currentYVelocity = currentYVelocity;
+	}
+
+	public double getCurrentZVelocity() {
+		return currentZVelocity;
+	}
+
+	public void setCurrentZVelocity(double currentZVelocity) {
+		this.currentZVelocity = currentZVelocity;
+	}
+
+}
