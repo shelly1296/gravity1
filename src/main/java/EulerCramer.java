658e9475c664ddd2a55991367f46de3a29b2a273
Adam Shelbourne <a.shelbourne@lancaster.ac.uk>
2016-12-10 13:46:26 +0000

    Corrected coding style

--- src/main/java/EulerCramer.java
+++ src/main/java/EulerCramer.java
@@ -10,7 +10,7 @@ public class EulerCramer {
 
 	private static final Double ZERO = Double.valueOf("0.0");
 	private static final Double AU = 1.49597870700E12;
-	private static final Double OrbitalSpeed = 2.978589E4;
+	private static final Double ORBITAL_SPEED = 2.978589E4;
 	private static final Double HOUR = 60.0 * 60.0;
 
 	public EulerCramer() {
@@ -22,7 +22,7 @@ public class EulerCramer {
 		ThreeDimensionVector tdv = new ThreeDimensionVector();
 		tdv.setyAxis(AU);
 		ThreeDimensionVector tdv1 = new ThreeDimensionVector();
-		tdv1.setxAxis(OrbitalSpeed);
+		tdv1.setxAxis(ORBITAL_SPEED);
 		for (int i = 0; i < 10000000; i++) {
 			tdv1 = tdv1.increaseBy(cgf.calculateGraviation(tdv).scale(1.0));
 			tdv = tdv.increaseBy(tdv1.scale(1.0));
