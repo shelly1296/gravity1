63c596e9675a944377dc5bb222f8539bab9a20cf
Adam Shelbourne <a.shelbourne@lancaster.ac.uk>
2016-12-10 14:11:45 +0000

    Adding structure to print data out.

--- src/main/java/GravityApp.java
+++ src/main/java/GravityApp.java
@@ -6,44 +6,52 @@ import java.util.List;
  *
  */
 public class GravityApp {
-	private static final Double DAY = 60.0*60.0*24.0;
+	private static final Double DAY = 60.0 * 60.0 * 24.0;
 	private static final Double YEAR = DAY * 365;
 
-	List<CelestialBody> celestialBodies = new ArrayList<CelestialBody>();
-	
+	List<CelestialBody> celestialBodies = new ArrayList<>();
+	List<PlanetDataWriter> writers = new ArrayList<>();
+
 	public GravityApp() {
-		for(PlanetData pd : PlanetData.values() ) {
+		for (PlanetData pd : PlanetData.values()) {
 			CelestialBody cb = new CelestialBody(pd);
 			celestialBodies.add(cb);
 		}
 	}
-	
 
 	public void moveCelestialBodiesOneDay() {
 		CalculateGravitaionalField cfg = new CalculateGravitaionalField();
-		for( CelestialBody cb : celestialBodies) {
+		for (CelestialBody cb : celestialBodies) {
 			// do maths to move it by one day...
 			ThreeDimensionVector positionTdv = cb.getPositionVector();
 			ThreeDimensionVector velocityTdv = cb.getVelocityVector();
 			ThreeDimensionVector gravity = cfg.calculateGraviation(positionTdv);
 			velocityTdv = velocityTdv.increaseBy(gravity.scale(DAY));
 			positionTdv = positionTdv.increaseBy(velocityTdv.scale(DAY));
-			System.out.println("(" + positionTdv.getxAxis() + ", " + positionTdv.getyAxis() + ", " + positionTdv.getzAxis() + ")");
+			System.out.println(
+					"(" + positionTdv.getxAxis() + ", " + positionTdv.getyAxis() + ", " + positionTdv.getzAxis() + ")");
 		}
 	}
-	
+
 	public void moveCelestialBodiesOneYear() {
 		EulerCramer ec = new EulerCramer();
-		for( CelestialBody cb : celestialBodies) {
+		for (CelestialBody cb : celestialBodies) {
 			// do maths to move it by one day...
 			ThreeDimensionVector tdv = cb.getPositionVector();
 			ThreeDimensionVector tdv1 = cb.getVelocityVector();
 			ec.applyMethodSolarSystem(tdv, tdv1, YEAR);
 		}
 	}
+
+	private void printResults(CelestialBody cb) {
+		for (PlanetDataWriter pdw : writers) {
+			pdw.print(cb);
+		}
+	}
+
 	/**
-	 * Method performs the calculation for the projectile in the earths gravitaional
-	 * field
+	 * Method performs the calculation for the projectile in the earths
+	 * gravitaional field
 	 */
 	private void performCalculate() {
 		GravityScanner gs = new GravityScanner();
@@ -51,9 +59,10 @@ public class GravityApp {
 		Projectile projectile = new Projectile(scannerDto);
 		projectile.calculate();
 	}
-    /**
-     * 
-     */
+
+	/**
+	 * 
+	 */
 	private void performMethod() {
 		EulerCramer eulerCramer = new EulerCramer();
 		eulerCramer.applyMethod();
@@ -62,7 +71,8 @@ public class GravityApp {
 	/**
 	 * The main Method for the class and the app
 	 * 
-	 * @param args -- the command line arguments
+	 * @param args
+	 *            -- the command line arguments
 	 */
 
 	public static void main(String[] args) {
